1e2b422efc15bfa0e0d714bf40cf38bd3c88d544
Thor Nissen <thorjnissen@gmail.com>
2024-04-17 14:43:08 +0200

    Translated account.php

--- packages/admin/resources/lang/da/account.php
+++ packages/admin/resources/lang/da/account.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'title' => '',
-    'save_btn' => '',
-    'avatar_notice' => '',
-    'view-profile' => '',
-    'update_password' => '',
-    'logout' => '',
+    'title' => 'Konto',
+    'save_btn' => 'Gem detaljer',
+    'avatar_notice' => 'Din avatar er genereret af Gravatar, via din e-mail adresse.',
+    'view-profile' => 'Vis profil',
+    'update_password' => 'Opdatér password',
+    'logout' => 'Log ud',
 ];
